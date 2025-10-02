339138b8dd85f6cd37b31447b67478b09cc1e8a6
Evan Huizinga <156137983+evanhuizinga@users.noreply.github.com>
2025-10-02 16:15:19 -0600

    Create JeopardySong.c

---
+++ lab06/JeopardySong.c
@@ -0,0 +1,480 @@
+/* Jeopardy Song */
+  Song[0].note = A4;
+  Song[0].size = quarter;
+  Song[0].tempo = 1400;
+  Song[0].space = 10;
+  Song[0].end = 0;
+
+  Song[1].note = D5;
+  Song[1].size = quarter;
+  Song[1].tempo = 1400;
+  Song[1].space = 10;
+  Song[1].end = 0;
+
+  Song[2].note = A4;
+  Song[2].size = quarter;
+  Song[2].tempo = 1400;
+  Song[2].space = 10;
+  Song[2].end = 0;
+
+  Song[3].note = D4;
+  Song[3].size = quarter;
+  Song[3].tempo = 1400;
+  Song[3].space = 10;
+  Song[3].end = 0;
+
+  Song[4].note = A4;
+  Song[4].size = quarter;
+  Song[4].tempo = 1400;
+  Song[4].space = 10;
+  Song[4].end = 0;
+
+  Song[5].note = D5;
+  Song[5].size = quarter;
+  Song[5].tempo = 1400;
+  Song[5].space = 10;
+  Song[5].end = 0;
+
+  Song[6].note = A4;
+  Song[6].size = quarter;
+  Song[6].tempo = 1400;
+  Song[6].space = 10;
+  Song[6].end = 0;
+
+  Song[7].note = rest;
+  Song[7].size = quarter;
+  Song[7].tempo = 1400;
+  Song[7].space = 10;
+  Song[7].end = 0;
+
+  Song[8].note = A4;
+  Song[8].size = quarter;
+  Song[8].tempo = 1400;
+  Song[8].space = 10;
+  Song[8].end = 0;
+
+  Song[9].note = D5;
+  Song[9].size = quarter;
+  Song[9].tempo = 1400;
+  Song[9].space = 10;
+  Song[9].end = 0;
+
+  Song[10].note = A4;
+  Song[10].size = quarter;
+  Song[10].tempo = 1400;
+  Song[10].space = 10;
+  Song[10].end = 0;
+
+  Song[11].note = D5;
+  Song[11].size = quarter;
+  Song[11].tempo = 1400;
+  Song[11].space = 10;
+  Song[11].end = 0;
+
+  Song[12].note = Fs5_Gb5;
+  Song[12].size = quarter;
+  Song[12].tempo = 1400;
+  Song[12].space = 100;
+  Song[12].end = 0;
+
+  Song[13].note = rest;
+  Song[13].size = _8th;
+  Song[13].tempo = 1400;
+  Song[13].space = 10;
+  Song[13].end = 0;
+
+  Song[14].note = E5;
+  Song[14].size = _8th;
+  Song[14].tempo = 1400;
+  Song[14].space = 10;
+  Song[14].end = 0;
+
+  Song[15].note = D5;
+  Song[15].size = _8th;
+  Song[15].tempo = 1400;
+  Song[15].space = 10;
+  Song[15].end = 0;
+
+  Song[16].note = Cs5_Db5;
+  Song[16].size = _8th;
+  Song[16].tempo = 1400;
+  Song[16].space = 10;
+  Song[16].end = 0;
+
+  Song[17].note = B4;
+  Song[17].size = _8th;
+  Song[17].tempo = 1400;
+  Song[17].space = 10;
+  Song[17].end = 0;
+
+  Song[18].note = As4_Bb4;
+  Song[18].size = _8th;
+  Song[18].tempo = 1400;
+  Song[18].space = 10;
+  Song[18].end = 0;
+
+  Song[19].note = A4;
+  Song[19].size = quarter;
+  Song[19].tempo = 1400;
+  Song[19].space = 10;
+  Song[19].end = 0;
+
+  Song[20].note = D5;
+  Song[20].size = quarter;
+  Song[20].tempo = 1400;
+  Song[20].space = 10;
+  Song[20].end = 0;
+
+  Song[21].note = A4;
+  Song[21].size = quarter;
+  Song[21].tempo = 1400;
+  Song[21].space = 10;
+  Song[21].end = 0;
+
+  Song[22].note = Fs4_Gb4;
+  Song[22].size = _8th;
+  Song[22].tempo = 1400;
+  Song[22].space = 10;
+  Song[22].end = 0;
+
+  Song[23].note = G4;
+  Song[23].size = _8th;
+  Song[23].tempo = 1400;
+  Song[23].space = 10;
+  Song[23].end = 0;
+
+  Song[24].note = A4;
+  Song[24].size = quarter;
+  Song[24].tempo = 1400;
+  Song[24].space = 10;
+  Song[24].end = 0;
+
+  Song[25].note = D5;
+  Song[25].size = quarter;
+  Song[25].tempo = 1400;
+  Song[25].space = 10;
+  Song[25].end = 0;
+
+  Song[26].note = A4;
+  Song[26].size = quarter;
+  Song[26].tempo = 1400;
+  Song[26].space = 10;
+  Song[26].end = 0;
+
+  Song[27].note = rest;
+  Song[27].size = quarter;
+  Song[27].tempo = 1400;
+  Song[27].space = 10;
+  Song[27].end = 0;
+
+  Song[28].note = D5;
+  Song[28].size = quarter;
+  Song[28].tempo = 1400;
+  Song[28].space = 100;
+  Song[28].end = 0;
+
+  Song[29].note = rest;
+  Song[29].size = _8th;
+  Song[29].tempo = 1400;
+  Song[29].space = 10;
+  Song[29].end = 0;
+
+  Song[30].note = B4;
+  Song[30].size = _8th;
+  Song[30].tempo = 1400;
+  Song[30].space = 10;
+  Song[30].end = 0;
+
+  Song[31].note = A4;
+  Song[31].size = quarter;
+  Song[31].tempo = 1400;
+  Song[31].space = 100;
+  Song[31].end = 0;
+
+  Song[32].note = G4;
+  Song[32].size = quarter;
+  Song[32].tempo = 1400;
+  Song[32].space = 100;
+  Song[32].end = 0;
+
+  Song[33].note = Fs4_Gb4;
+  Song[33].size = quarter;
+  Song[33].tempo = 1400;
+  Song[33].space = 100;
+  Song[33].end = 0;
+
+  Song[34].note = E4;
+  Song[34].size = quarter;
+  Song[34].tempo = 1400;
+  Song[34].space = 100;
+  Song[34].end = 0;
+
+  Song[35].note = D4;
+  Song[35].size = quarter;
+  Song[35].tempo = 1400;
+  Song[35].space = 100;
+  Song[35].end = 0;
+
+  Song[36].note = rest;
+  Song[36].size = quarter;
+  Song[36].tempo = 1400;
+  Song[36].space = 10;
+  Song[36].end = 0;
+
+  Song[37].note = C5;
+  Song[37].size = quarter;
+  Song[37].tempo = 1400;
+  Song[37].space = 10;
+  Song[37].end = 0;
+
+  Song[38].note = F5;
+  Song[38].size = quarter;
+  Song[38].tempo = 1400;
+  Song[38].space = 10;
+  Song[38].end = 0;
+
+  Song[39].note = C5;
+  Song[39].size = quarter;
+  Song[39].tempo = 1400;
+  Song[39].space = 10;
+  Song[39].end = 0;
+
+  Song[40].note = F4;
+  Song[40].size = _8th;
+  Song[40].tempo = 1400;
+  Song[40].space = 10;
+  Song[40].end = 0;
+
+  Song[41].note = F4;
+  Song[41].size = _8th;
+  Song[41].tempo = 1400;
+  Song[41].space = 10;
+  Song[41].end = 0;
+
+  Song[42].note = C5;
+  Song[42].size = quarter;
+  Song[42].tempo = 1400;
+  Song[42].space = 10;
+  Song[42].end = 0;
+
+  Song[43].note = F5;
+  Song[43].size = quarter;
+  Song[43].tempo = 1400;
+  Song[43].space = 10;
+  Song[43].end = 0;
+
+  Song[44].note = C5;
+  Song[44].size = quarter;
+  Song[44].tempo = 1400;
+  Song[44].space = 10;
+  Song[44].end = 0;
+
+  Song[45].note = rest;
+  Song[45].size = quarter;
+  Song[45].tempo = 1400;
+  Song[45].space = 10;
+  Song[45].end = 0;
+
+  Song[46].note = C5;
+  Song[46].size = quarter;
+  Song[46].tempo = 1400;
+  Song[46].space = 10;
+  Song[46].end = 0;
+
+  Song[47].note = F5;
+  Song[47].size = quarter;
+  Song[47].tempo = 1400;
+  Song[47].space = 10;
+  Song[47].end = 0;
+
+  Song[48].note = C5;
+  Song[48].size = quarter;
+  Song[48].tempo = 1400;
+  Song[48].space = 10;
+  Song[48].end = 0;
+
+  Song[49].note = F5;
+  Song[49].size = quarter;
+  Song[49].tempo = 1400;
+  Song[49].space = 10;
+  Song[49].end = 0;
+
+  Song[50].note = A5;
+  Song[50].size = quarter;
+  Song[50].tempo = 1400;
+  Song[50].space = 0;
+  Song[50].end = 0;
+
+  Song[51].note = A5;
+  Song[51].size = _8th;
+  Song[51].tempo = 1400;
+  Song[51].space = 10;
+  Song[51].end = 0;
+
+  Song[52].note = G5;
+  Song[52].size = _8th;
+  Song[52].tempo = 1400;
+  Song[52].space = 10;
+  Song[52].end = 0;
+
+  Song[53].note = F5;
+  Song[53].size = _8th;
+  Song[53].tempo = 1400;
+  Song[53].space = 10;
+  Song[53].end = 0;
+
+  Song[54].note = E5;
+  Song[54].size = _8th;
+  Song[54].tempo = 1400;
+  Song[54].space = 10;
+  Song[54].end = 0;
+
+  Song[55].note = D5;
+  Song[55].size = _8th;
+  Song[55].tempo = 1400;
+  Song[55].space = 10;
+  Song[55].end = 0;
+
+  Song[56].note = Cs5_Db5;
+  Song[56].size = _8th;
+  Song[56].tempo = 1400;
+  Song[56].space = 10;
+  Song[56].end = 0;
+
+  Song[57].note = C5;
+  Song[57].size = quarter;
+  Song[57].tempo = 1400;
+  Song[57].space = 10;
+  Song[57].end = 0;
+
+  Song[58].note = F5;
+  Song[58].size = quarter;
+  Song[58].tempo = 1400;
+  Song[58].space = 10;
+  Song[58].end = 0;
+
+  Song[59].note = C5;
+  Song[59].size = quarter;
+  Song[59].tempo = 1400;
+  Song[59].space = 10;
+  Song[59].end = 0;
+
+  Song[60].note = A4;
+  Song[60].size = _8th;
+  Song[60].tempo = 1400;
+  Song[60].space = 10;
+  Song[60].end = 0;
+
+  Song[61].note = As4_Bb4;
+  Song[61].size = _8th;
+  Song[61].tempo = 1400;
+  Song[61].space = 10;
+  Song[61].end = 0;
+
+  Song[62].note = C5;
+  Song[62].size = quarter;
+  Song[62].tempo = 1400;
+  Song[62].space = 10;
+  Song[62].end = 0;
+
+  Song[63].note = F5;
+  Song[63].size = quarter;
+  Song[63].tempo = 1400;
+  Song[63].space = 10;
+  Song[63].end = 0;
+
+  Song[64].note = C5;
+  Song[64].size = quarter;
+  Song[64].tempo = 1400;
+  Song[64].space = 10;
+  Song[64].end = 0;
+
+  Song[65].note = rest;
+  Song[65].size = _16th;
+  Song[65].tempo = 1400;
+  Song[65].space = 10;
+  Song[65].end = 0;
+
+  Song[66].note = C5;
+  Song[66].size = _16th;
+  Song[66].tempo = 1400;
+  Song[66].space = 10;
+  Song[66].end = 0;
+
+  Song[67].note = D5;
+  Song[67].size = _16th;
+  Song[67].tempo = 1400;
+  Song[67].space = 10;
+  Song[67].end = 0;
+
+  Song[68].note = E5;
+  Song[68].size = _16th;
+  Song[68].tempo = 1400;
+  Song[68].space = 10;
+  Song[68].end = 0;
+
+  Song[69].note = F5;
+  Song[69].size = quarter;
+  Song[69].tempo = 1400;
+  Song[69].space = 100;
+  Song[69].end = 0;
+
+  Song[70].note = rest;
+  Song[70].size = _8th;
+  Song[70].tempo = 1400;
+  Song[70].space = 10;
+  Song[70].end = 0;
+
+  Song[71].note = D5;
+  Song[71].size = _8th;
+  Song[71].tempo = 1400;
+  Song[71].space = 10;
+  Song[71].end = 0;
+
+  Song[72].note = C5;
+  Song[72].size = quarter;
+  Song[72].tempo = 1400;
+  Song[72].space = 100;
+  Song[72].end = 0;
+
+  Song[73].note = As4_Bb4;
+  Song[73].size = quarter;
+  Song[73].tempo = 1400;
+  Song[73].space = 100;
+  Song[73].end = 0;
+
+  Song[74].note = A4;
+  Song[74].size = quarter;
+  Song[74].tempo = 1400;
+  Song[74].space = 100;
+  Song[74].end = 0;
+
+  Song[75].note = rest;
+  Song[75].size = quarter;
+  Song[75].tempo = 1400;
+  Song[75].space = 100;
+  Song[75].end = 0;
+
+  Song[76].note = G4;
+  Song[76].size = quarter;
+  Song[76].tempo = 1400;
+  Song[76].space = 100;
+  Song[76].end = 0;
+
+  Song[77].note = rest;
+  Song[77].size = quarter;
+  Song[77].tempo = 1400;
+  Song[77].space = 100;
+  Song[77].end = 0;
+
+  Song[78].note = F4;
+  Song[78].size = quarter;
+  Song[78].tempo = 1400;
+  Song[78].space = 100;
+  Song[78].end = 0;
+
+  Song[99].note = rest;
+  Song[99].size = quarter;
+  Song[99].tempo = 1400;
+  Song[99].space = 10;
+  Song[99].end = 1;
